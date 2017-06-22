3f69e1753fbd32dd53e0532e194e073f4447a43b
g0z4ru <pmeiclak@docomo.ne.jp>
2017-06-22 22:09:38 +0900

    説明のコメント追加

--- src/sample/Rational.kt
+++ src/sample/Rational.kt
@@ -5,8 +5,12 @@ class Rational(n: Int, d: Int) {
         require(d != 0, {"denominator must not be null"})
     }
     private val g = gcd(Math.abs(n), Math.abs(d))
+
+    // 呼び出された時に値が確定する
     val numerator: Int by lazy { n / g }
     val denominator: Int by lazy { d / g }
+
+    //　operatorと一緒に決められたメソッド名で実装することによって演算子をオーバーロードできる
     operator fun plus(that: Rational): Rational =
     Rational(
             numerator * that.denominator + that.numerator * denominator,
