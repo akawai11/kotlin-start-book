c0ef047057c58b9ab67ffdcc05e370a27dc5ba5b
g0z4ru <pmeiclak@docomo.ne.jp>
2017-06-27 23:22:24 +0900

    配列処理

--- src/sample/Object.kt
+++ src/sample/Object.kt
@@ -3,6 +3,7 @@ package sample
 /* 様々なオブジェクト */
 fun main(args: Array<String>) {
 
+    // 文字列処理
     var name = "g0z4ru"
     println(name.length)
     println(name.capitalize())
@@ -21,4 +22,17 @@ fun main(args: Array<String>) {
     """
     println(rawString)
     println(rawString.trimMargin())
+
+    // 配列
+    var ints = arrayOfNulls<Int>(5)
+    println(ints.size)
+    println(ints[0])
+    ints[0] = 123
+    println(ints[0])
+    val strs = arrayOf("red", "green", "blue")
+    println(strs[0])
+    val intArr: IntArray = intArrayOf(1, 2, 3)
+    println(intArr[0])
+    val chars: CharArray = charArrayOf('a', 'b', 'c')
+    println("${chars[0]}, ${chars[1]}")
 }
