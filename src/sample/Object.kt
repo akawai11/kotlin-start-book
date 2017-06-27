f7d4bc2040f6d9cac91d36411ff42040824fb9dd
g0z4ru <pmeiclak@docomo.ne.jp>
2017-06-28 00:44:07 +0900

    レンジ処理

--- src/sample/Object.kt
+++ src/sample/Object.kt
@@ -71,11 +71,16 @@ fun main(args: Array<String>) {
     println(numberMap)
 
     // レンジ
-    print("5 in 1..10 :")
+    print("5 in 1..10 : ")
     println(5 in 1..10)
     val range: IntRange = 12..15
-    print("5 in range")
+    print("5 in range : ")
     println(5 in range)
-    print("5 !in range")
+    print("5 !in range : ")
     println(5 !in range)
+    println((1..5).toList())
+    println((1..5).reversed().toList())
+    println((5 downTo 1).toList())
+    println((1..5 step 2).toList())
+    println((100 downTo 0 step 25).toList())
 }
