c89257143ac043f5ee7ca96ba296675bc11beef2
g0z4ru <pmeiclak@docomo.ne.jp>
2017-10-23 23:49:05 +0900

    カスタムセッターを作成

--- src/sample/Property.kt
+++ src/sample/Property.kt
@@ -7,6 +7,17 @@ class Person {
     get(): Int {
         return this.name.length
     }
+
+    var name2: String = ""
+    set(value) {
+        println("${value}がsetされました")
+        field = value
+    }
+
+    val nameLength2: Int
+        get(): Int {
+            return this.name.length
+        }
 }
 
 fun main(args: Array<String>) {
@@ -21,4 +32,9 @@ fun main(args: Array<String>) {
     println(hanako.name) // はなこを出力
     println(hanako.age) // 25を出力
     println(hanako.nameLength)
+
+
+    hanako.name2 = "はなこ2"
+    println(hanako.name2)
+    println(hanako.nameLength2)
 }
